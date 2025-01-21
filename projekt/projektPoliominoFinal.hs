3d1b7bcdb020ba61ed62a3e7848921c89994fb6c
Panstick <tymoteusz.blaszcz@student.uj.edu.pl>
2025-01-21 13:55:46 +0100

    projekt final final final final final

--- projekt/projektPoliominoFinal.hs
+++ projekt/projektPoliominoFinal.hs
@@ -1,6 +1,6 @@
 type Point = (Int, Int)
 type Polyomino = [Point]
-    
+
 sort :: (Ord a) => [a] -> [a]
 sort [] = []
 sort (x:xs) = sort (filter (< x) xs) ++ [x] ++ sort (filter (> x) xs)
@@ -10,6 +10,29 @@ normalize x = map (\(x, y) -> (x-dx, y-dy)) x
   where
     dx = minimum (map fst x)
     dy = minimum (map snd x)
+
+earlierInGrid :: Point -> Point -> Bool
+earlierInGrid (x1, y1) (x2, y2)
+  | y1 == y2 = x1 > x2
+  | y1 < y2 = True
+  | y1 > y2 = False
+
+sortGrid :: [Point] -> [Point]
+sortGrid [] = []
+sortGrid (x:xs) = sortGrid (filter (earlierInGrid x) xs) ++ [x] ++ sortGrid (filter (not . earlierInGrid x) xs)
+
+addNewLines :: Int -> String -> String
+addNewLines _ [] = "\n"
+addNewLines n str = take n str ++ "\n" ++ addNewLines n (drop n str)
+
+generateGrid :: Int -> Int -> [Point]
+generateGrid x y = [(x, y) | x <- [0..x], y <- [0..y]]
+
+parsePolyomino :: Polyomino -> String
+parsePolyomino x = addNewLines (dx+1) (map (\a -> if a `elem` x then '*' else ' ') (sortGrid $ generateGrid dx dy))
+  where
+    dx = maximum (map fst x)  
+    dy = maximum (map snd x)
 
 checkPolyomino :: Int -> [Point] -> [Point] -> Polyomino -> [Polyomino]
 checkPolyomino 0  _ _ child = [child]
@@ -25,8 +48,9 @@ tryAllNeighbours _ [] _ _ = []
 tryAllNeighbours val (x:xs) restrictedPoints parent =
      checkPolyomino (val-1) xs (x : restrictedPoints) (x : parent) ++ tryAllNeighbours val xs (x : restrictedPoints) parent
 
-generatePolyominoes :: Int -> [Polyomino]
-generatePolyominoes x = sort $ map (sort . normalize) (tryAllNeighbours x [(0,0)] [] [])
+--sort wykonany na liscie poliomin uzywam jako usuniecie powtorek
+generatePolyominoes :: Int -> String
+generatePolyominoes x = concatMap parsePolyomino (sort $ map (sort . normalize) (tryAllNeighbours x [(0,0)] [] []))
 
 countPolyominoes :: Int -> Int
 countPolyominoes x = div (length $ tryAllNeighbours x [(0,0)] [] []) x
